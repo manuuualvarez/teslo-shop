40cc9b797fd45619ad84ce32d8a02e5030dafcae
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2022-09-08 20:26:09 -0300

    add meta tags and layout

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,44 @@
+import Head from 'next/head'
+import React, { FC } from 'react'
+
+interface Props {
+    title: string;
+    pageDescription: string;
+    imgFullURL?: string;
+    children?: React.ReactNode | undefined
+}
+
+export const ShopLayout: FC<Props> = ({ children, pageDescription, title, imgFullURL }) => {
+  return (
+    <>
+        <Head>
+            <title>{ title }</title>
+            <meta name='description' content={ pageDescription}/>
+            <meta name='og:title' content={ title }/>
+            <meta name='og:description' content={ pageDescription }/>
+
+            {
+                imgFullURL && (
+                    <meta name='og:image' content={ imgFullURL }/>
+                )
+            }
+        </Head>
+
+        <nav>
+            {/* TODO: Navbar */}
+        </nav>
+
+        {/* SideBar */}
+
+        <main style={{ margin: '80px auto', maxWidth: '1440px', padding: '0px 30px'}}>
+            { children }
+        </main>
+
+        <footer>
+            {/* Custom footer */}
+        </footer>
+    </>
+  )
+}
+
+
