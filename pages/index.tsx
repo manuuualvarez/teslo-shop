7e50af99c3b48ab8e6a20ca431ffa64b2ea00995
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2022-09-08 21:09:14 -0300

    show mock products

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,37 @@
-import { Typography } from '@mui/material'
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
 import type { NextPage } from 'next'
 import { ShopLayout }  from '../components/layouts'
+import { initialData } from '../database/products';
+
+
+
 
 const Home: NextPage = () => {
   return (
     <ShopLayout title={'Teslo Shop - Home'} pageDescription={'Find the best product of TESLO'}>
       <Typography variant='h1' component='h1'>Shop</Typography>
       <Typography variant='h2' sx={{mb: 1}}>All Products</Typography>
+
+      <Grid container spacing={4}>
+          {
+            initialData.products.map(product => (
+              <Grid item xs={6} sm={4} key={product.slug}>
+                <Card>
+                  <CardActionArea>
+                    <CardMedia
+                      component='img'
+                      image={`products/${product.images[0]}`}
+                      alt={product.title}
+                    >
+
+                    </CardMedia>
+                  </CardActionArea>
+                </Card>
+
+              </Grid>
+            ))
+          }
+      </Grid>
 
     </ShopLayout>
   )
