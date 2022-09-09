ee05b5a2b7144e9970d91bcdbda10a50d17301be
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2022-09-08 21:17:45 -0300

    add sidebar

--- components/layouts/ShopLayout.tsx
+++ components/layouts/ShopLayout.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import React, { FC } from 'react'
-import { Navbar } from '../ui';
+import { Navbar, SideMenu } from '../ui';
 
 interface Props {
     title: string;
@@ -29,7 +29,7 @@ export const ShopLayout: FC<Props> = ({ children, pageDescription, title, imgFul
             <Navbar></Navbar>
         </nav>
 
-        {/* SideBar */}
+        <SideMenu></SideMenu>
 
         <main style={{ margin: '80px auto', maxWidth: '1440px', padding: '0px 30px'}}>
             { children }
