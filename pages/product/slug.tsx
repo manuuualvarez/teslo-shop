c09f102bb38adc00043f97dffc1cc93862d71437
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2022-09-09 21:34:49 -0300

    add a counter

--- pages/product/slug.tsx
+++ pages/product/slug.tsx
@@ -4,32 +4,35 @@ import { ShopLayout } from '../../components/layouts'
 import { initialData } from '../../database/products';
 import { ProductSlideshow } from '../../components/products';
 import 'react-slideshow-image/dist/styles.css'
+import { ItemCounter } from '../../components/ui';
 
 const product = initialData.products[0];
 
 const ProductPage = () => {
   return (
     <ShopLayout title={ product.title } pageDescription={ product.description }>
-
       <Grid container spacing={3}>
         <Grid item xs={12} sm={7}>
-          
           <ProductSlideshow  images={product.images}/>
         </Grid>
+
         <Grid item xs={12} sm={5}>
           <Box display={'flex'} flexDirection={'column'}>
             {/* Titles */}
             <Typography variant='h1' component={'h1'}> { product.title }</Typography>
             <Typography variant='subtitle1' component={'h2'}> ${ product.price }</Typography>
+            
             {/* Quantity */}
             <Box sx={{my: 2}}>
               <Typography variant='subtitle2'>Quantity</Typography>
-              {/* Item Counter  TODO*/}
+              <ItemCounter/>
             </Box>
+
             {/* ADD Cart */}
             <Button color='secondary' className='circular-btn'>
               Add to Cart
             </Button>
+
             {/* Out of stock */}
             {/* <Chip label="Not available" color='error' variant='outlined'/> */}
 
@@ -37,14 +40,10 @@ const ProductPage = () => {
             <Box sx={{mt: 3}}>
               <Typography variant='subtitle2'>Description</Typography>
               <Typography variant='body2'>{ product.description }</Typography>
-
             </Box>
-
-            
           </Box>
         </Grid>
       </Grid>
-
     </ShopLayout>
   )
 }
