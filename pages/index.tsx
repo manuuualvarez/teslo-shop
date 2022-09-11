a3a50fcf4f2b96b6538b41788a609c625127b557
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2022-09-11 11:49:31 -0300

    add fetch howw with swr and loding view

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,24 @@ import { Typography } from '@mui/material'
 import type { NextPage } from 'next'
 import { ShopLayout }  from '../components/layouts'
 import { ProductList } from '../components/products';
-import { initialData } from '../database/products';
+import { FullScreenLoading } from '../components/ui';
+import { useProducts } from '../hooks';
 
 
 const Home: NextPage = () => {
+
+  const { products, isLoading, isError }  = useProducts('/products');
+
   return (
     <ShopLayout title={'Teslo Shop - Home'} pageDescription={'Find the best product of TESLO'}>
       <Typography variant='h1' component='h1'>Shop</Typography>
       <Typography variant='h2' sx={{mb: 1}}>All Products</Typography>
 
-      <ProductList products={ initialData.products as any }/>
-
+      {
+        isLoading
+        ? <FullScreenLoading/>
+        : <ProductList products={ products }/>
+      }
     </ShopLayout>
   )
 }
