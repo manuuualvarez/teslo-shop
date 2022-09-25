cc567e6dc3f2defddb4e88602130d234b6170588
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2022-09-25 01:29:20 -0300

    update product form and rest api

--- pages/admin/products/[slug].tsx
+++ pages/admin/products/[slug].tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { GetServerSideProps } from 'next'
 
 import { IProduct } from '../../../interfaces';
@@ -6,11 +6,28 @@ import { DriveFileRenameOutline, SaveOutlined, UploadOutlined } from '@mui/icons
 import { dbProducts } from '../../../database';
 import { Box, Button, capitalize, Card, CardActions, CardMedia, Checkbox, Chip, Divider, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, ListItem, Paper, Radio, RadioGroup, TextField } from '@mui/material';
 import { AddminLayout } from '../../../components/layouts';
+import { useForm } from 'react-hook-form';
+import tesloApi from '../../../api/tesloApi';
 
 
 const validTypes  = ['shirts','pants','hoodies','hats']
 const validGender = ['men','women','kid','unisex']
 const validSizes = ['XS','S','M','L','XL','XXL','XXXL']
+
+
+interface FormData {
+    _id?: string;
+    description: string;
+    images: string[];
+    inStock: number;
+    price: number;
+    sizes: string[];
+    slug: string;
+    tags: string[];
+    title: string;
+    type: string;
+    gender: string;
+}
 
 interface Props {
     product: IProduct;
@@ -18,26 +35,98 @@ interface Props {
 
 const ProductAdminPage:FC<Props> = ({ product }) => {
 
+    const { register, handleSubmit, formState: { errors}, getValues, setValue, watch } = useForm<FormData>({
+        defaultValues: product
+    })
+
+    const [newTagValue, setnewTagValue] = useState('');
+    const [isSaving, setIsSaving] = useState(false)
+
+    useEffect(() => {
+      const subsciption = watch(( value, {name, type}) => {
+        if(name === 'title') {
+            const newSlug = value.title?.trim()
+            .replaceAll(' ', '_')
+            .replaceAll("'", '')
+            .toLocaleLowerCase() || ''
+
+            setValue('slug', newSlug);
+        }
+      })
+      return () => subsciption.unsubscribe();
+    }, [watch, setValue])
+    
+
+    const onChangeSize = (size: string) => {
+        const currentSizes = getValues('sizes');
+        if(currentSizes.includes(size)){
+            return setValue('sizes', currentSizes.filter(s => s !== size), { shouldValidate: true })
+        }
+        setValue('sizes', [...currentSizes, size], { shouldValidate: true})
+    }
+    
+    const onNewTag = () => {
+        const newTag = newTagValue.trim().toLowerCase();
+        setnewTagValue('');
+
+        const currentTags = getValues('tags');
+
+        if(currentTags.includes(newTag)){
+            return;
+        }
+
+        currentTags.push(newTag);
+    }
+
     const onDeleteTag = ( tag: string ) => {
+        const updatedTags = getValues('tags').filter(t => t !== tag);
+        setValue('tags', updatedTags, { shouldValidate: true })
+
+    }
+
+    const onSubmitForm = async (form: FormData) => {
+        if (form.images.length < 2 && form.tags.length == 0) return;
+        setIsSaving(true);
+
+        try {
+            const { data } = await tesloApi({
+                url: '/admin/products',
+                method: 'PUT',
+                data: form
+            });
+
+            console.log("Se updeteo ok: ", data);
+
+            if(!form._id) {
+
+            } else {
+                setIsSaving(false);
+            }
+            
+        } catch (error) {
+            console.log(error);
+            setIsSaving(false);
+        }
 
     }
 
     return (
 
         <AddminLayout 
-            title={'Producto'} 
-            subtitle={`Editando: ${ product.title }`}
+            title={'Product'} 
+            subtitle={`Editing: ${ product.title }`}
             icon={ <DriveFileRenameOutline /> }
         >
-            <form>
+            <form onSubmit={ handleSubmit(onSubmitForm) }>
                 <Box display='flex' justifyContent='end' sx={{ mb: 1 }}>
                     <Button 
                         color="secondary"
                         startIcon={ <SaveOutlined /> }
                         sx={{ width: '150px' }}
                         type="submit"
+                        disabled={ isSaving }
                         >
-                        Guardar
+                        Save
                     </Button>
                 </Box>
 
@@ -46,50 +135,68 @@ const ProductAdminPage:FC<Props> = ({ product }) => {
                     <Grid item xs={12} sm={ 6 }>
 
                         <TextField
-                            label="Título"
+                            label="Title"
                             variant="filled"
                             fullWidth 
                             sx={{ mb: 1 }}
-                            // { ...register('name', {
-                            //     required: 'Este campo es requerido',
-                            //     minLength: { value: 2, message: 'Mínimo 2 caracteres' }
-                            // })}
-                            // error={ !!errors.name }
-                            // helperText={ errors.name?.message }
+                            { ...register('title', {
+                                required: 'This field is mandatory',
+                                minLength: { value: 2, message: 'Minimum 2 characters' }
+                            })}
+                            error={ !!errors.title }
+                            helperText={ errors.title?.message }
                         />
 
                         <TextField
-                            label="Descripción"
+                            label="Description"
                             variant="filled"
                             fullWidth 
                             multiline
                             sx={{ mb: 1 }}
+                            { ...register('description', {
+                                required: 'This field is mandatory',
+                                minLength: { value: 5, message: 'Minimum 5 characters' }
+                            })}
+                            error={ !!errors.description }
+                            helperText={ errors.description?.message }
                         />
 
                         <TextField
-                            label="Inventario"
+                            label="Stock"
                             type='number'
                             variant="filled"
                             fullWidth 
                             sx={{ mb: 1 }}
+                            { ...register('inStock', {
+                                required: 'This field is mandatory',
+                                min: { value: 0, message: 'Minimum is 0' }
+                            })}
+                            error={ !!errors.inStock }
+                            helperText={ errors.inStock?.message }
                         />
                         
                         <TextField
-                            label="Precio"
+                            label="Price"
                             type='number'
                             variant="filled"
                             fullWidth 
                             sx={{ mb: 1 }}
+                            { ...register('price', {
+                                required: 'This field is mandatory',
+                                min: { value: 0, message: 'Minimum is 0' }
+                            })}
+                            error={ !!errors.price }
+                            helperText={ errors.price?.message }
                         />
 
                         <Divider sx={{ my: 1 }} />
 
                         <FormControl sx={{ mb: 1 }}>
-                            <FormLabel>Tipo</FormLabel>
+                            <FormLabel>Type</FormLabel>
                             <RadioGroup
                                 row
-                                // value={ status }
-                                // onChange={ onStatusChanged }
+                                value={ getValues('type') }
+                                onChange={ ({target}) => setValue('type', target.value, { shouldValidate: true}) }
                             >
                                 {
                                     validTypes.map( option => (
@@ -105,11 +212,11 @@ const ProductAdminPage:FC<Props> = ({ product }) => {
                         </FormControl>
 
                         <FormControl sx={{ mb: 1 }}>
-                            <FormLabel>Género</FormLabel>
+                            <FormLabel>Gender</FormLabel>
                             <RadioGroup
                                 row
-                                // value={ status }
-                                // onChange={ onStatusChanged }
+                                value={ getValues('gender') }
+                                onChange={ ({target}) => setValue('gender', target.value, { shouldValidate: true}) }
                             >
                                 {
                                     validGender.map( option => (
@@ -125,31 +232,45 @@ const ProductAdminPage:FC<Props> = ({ product }) => {
                         </FormControl>
 
                         <FormGroup>
-                            <FormLabel>Tallas</FormLabel>
+                            <FormLabel>Sizes</FormLabel>
                             {
                                 validSizes.map(size => (
-                                    <FormControlLabel key={size} control={<Checkbox />} label={ size } />
+                                    <FormControlLabel 
+                                        key={size} 
+                                        control={<Checkbox checked={ getValues('sizes').includes(size) }/>} 
+                                        label={ size }
+                                        onChange={ () => onChangeSize(size)} 
+                                    />
                                 ))
                             }
                         </FormGroup>
 
                     </Grid>
 
-                    {/* Tags e imagenes */}
+                    
                     <Grid item xs={12} sm={ 6 }>
                         <TextField
                             label="Slug - URL"
                             variant="filled"
                             fullWidth
                             sx={{ mb: 1 }}
+                            { ...register('slug', {
+                                required: 'This field is mandatory',
+                                validate: (val) => val.trim().includes(' ') ? 'This field can not use blank spaces' : undefined
+                            })}
+                            error={ !!errors.slug }
+                            helperText={ errors.slug?.message }
                         />
-
+                        {/* Tags and images */}
                         <TextField
-                            label="Etiquetas"
+                            label="Tags"
                             variant="filled"
                             fullWidth 
                             sx={{ mb: 1 }}
-                            helperText="Presiona [spacebar] para agregar"
+                            helperText="Touch [spacebar] to add"
+                            value = { newTagValue }
+                            onChange={ ({target}) => setnewTagValue(target.value)}
+                            onKeyUp={ ({code}) => code === 'Space' ? onNewTag() : undefined }
                         />
                         
                         <Box sx={{
@@ -161,7 +282,7 @@ const ProductAdminPage:FC<Props> = ({ product }) => {
                         }}
                         component="ul">
                             {
-                                product.tags.map((tag) => {
+                                getValues('tags').map((tag) => {
 
                                 return (
                                     <Chip
